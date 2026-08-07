172283bf492c3e18714d21466eed24bff9bc3306
Brandon Duran <dm24001@ues.edu.sv>
2026-08-07 21:26:20 +0200

    Actualizar login.java

--- src/login.java
+++ src/login.java
@@ -2,6 +2,6 @@ class
     login {
         public static void main(String[] args) {
             System.out.println("Welcome to the login page!");
-            // Add your login logic here
+            // Add your login logic hee
         }
     }
