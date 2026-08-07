de49b03407fc1bd95d9079c5cf7ec47a806ca3d8
Duran <dm24001@ues.edu.sv>
2026-08-07 18:32:02 +0200

    SRC ADD

---
+++ src/login.java
@@ -0,0 +1,7 @@
+class
+    login {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the login page!");
+            // Add your login logic here
+        }
+    }
