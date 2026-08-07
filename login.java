982c5735f3d19ceb1aea26565972ae741eac1b8f
Duran <dm24001@ues.edu.sv>
2026-08-07 18:41:12 +0200

    LOGIN ELIMINADO

--- login.java
+++
@@ -1,7 +0,0 @@
-class
-    login {
-        public static void main(String[] args) {
-            System.out.println("Welcome to the login page!");
-            // Add your login logic here
-        }
-    }
